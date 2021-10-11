ee593f86a0617768868ac9c30817e96186f91361
sunilnarwaria <41354869+sunilnarwaria@users.noreply.github.com>
2021-10-11 15:24:32 +0530

    Update LRU cache.cpp

--- LRU cache.cpp
+++ LRU cache.cpp
@@ -1,88 +1,76 @@
-
-// We can use stl container list as a double 
-// ended queue to store the cache keys, with 
-// the descending time of reference from front 
-// to back and a set container to check presence 
-// of a key. But to fetch the address of the key 
-// in the list using find(), it takes O(N) time. 
-// This can be optimized by storing a reference 
-//	 (iterator) to each key in a hash map. 
-#include <bits/stdc++.h> 
-using namespace std; 
-
-class LRUCache { 
-	// store keys of cache 
-	list<int> dq; 
-
-	// store references of key in cache 
-	unordered_map<int, list<int>::iterator> ma; 
-	int csize; // maximum capacity of cache 
-
-public: 
-	LRUCache(int); 
-	void refer(int); 
-	void display(); 
-}; 
-
-// Declare the size 
-LRUCache::LRUCache(int n) 
-{ 
-	csize = n; 
-} 
-
-// Refers key x with in the LRU cache 
-void LRUCache::refer(int x) 
-{ 
-	// not present in cache 
-	if (ma.find(x) == ma.end()) { 
-		// cache is full 
-		if (dq.size() == csize) { 
-			// delete least recently used element 
-			int last = dq.back(); 
-
-			// Pops the last elmeent 
-			dq.pop_back(); 
-
-			// Erase the last 
-			ma.erase(last); 
-		} 
-	} 
-
-	// present in cache 
-	else
-		dq.erase(ma[x]); 
-
-	// update reference 
-	dq.push_front(x); 
-	ma[x] = dq.begin(); 
-} 
-
-// Function to display contents of cache 
-void LRUCache::display() 
-{ 
-
-	// Iterate in the deque and print 
-	// all the elements in it 
-	for (auto it = dq.begin(); it != dq.end(); 
-		it++) 
-		cout << (*it) << " "; 
-
-	cout << endl; 
-} 
-
-// Driver Code 
-int main() 
-{ 
-	LRUCache ca(4); 
-
-	ca.refer(1); 
-	ca.refer(2); 
-	ca.refer(3); 
-	ca.refer(1); 
-	ca.refer(4); 
-	ca.refer(5); 
-	ca.display(); 
-
-	return 0; 
-} 
-// This code is contributed by Satish Srinivas 
+class LRUCache {
+public:
+    class node {
+        public:
+            int key;
+            int val;
+            node* next;
+            node* prev;
+        node(int _key, int _val) {
+            key = _key;
+            val = _val; 
+        }
+    };
+    
+    node* head = new node(-1,-1);
+    node* tail = new node(-1,-1);
+    
+    int cap;
+    unordered_map<int, node*>m;
+    
+    LRUCache(int capacity) {
+        cap = capacity;    
+        head->next = tail;
+        tail->prev = head;
+    }
+    
+    void addnode(node* newnode) {
+        node* temp = head->next;
+        newnode->next = temp;
+        newnode->prev = head;
+        head->next = newnode;
+        temp->prev = newnode;
+    }
+    
+    void deletenode(node* delnode) {
+        node* delprev = delnode->prev;
+        node* delnext = delnode->next;
+        delprev->next = delnext;
+        delnext->prev = delprev;
+    }
+    
+    int get(int key_) {
+        if (m.find(key_) != m.end()) {
+            node* resnode = m[key_];
+            int res = resnode->val;
+            m.erase(key_);
+            deletenode(resnode);
+            addnode(resnode);
+            m[key_] = head->next;
+            return res; 
+        }
+    
+        return -1;
+    }
+    
+    void put(int key_, int value) {
+        if(m.find(key_) != m.end()) {
+            node* existingnode = m[key_];
+            m.erase(key_);
+            deletenode(existingnode);
+        }
+        if(m.size() == cap) {
+          m.erase(tail->prev->key);
+          deletenode(tail->prev);
+        }
+        
+        addnode(new node(key_, value));
+        m[key_] = head->next; 
+    }
+};
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
